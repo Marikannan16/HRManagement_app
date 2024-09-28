de1cc07c9d0d51e51e95f23393a5af848b52626f
Dinesh-s15 <dinesh.nathan2020@gmail.com>
2024-09-28 17:57:44 +0530

    Add files via upload

---
+++ dinesh/Createsublist/Scol.js
@@ -0,0 +1,42 @@
+import ActionMenu from './Act';
+const columns =[
+   
+    {
+      name:'S NO',
+      selector: row=>row.sno,
+      sortable:true
+    },
+    {
+      name:'Activity',
+      selector: row=>row.activity,
+      sortable:true
+    },
+    {
+      name:'Applicable Labour Act',
+      selector: row=>row.applicablelabouract,
+      sortable:true
+    },
+    {
+        name:'Type of Act',
+        selector: row=>row.typeofact,
+        sortable:true
+    },
+    {
+        name:'Frequency',
+        selector: row=>row.frequency,
+        sortable:true
+    },
+    {
+        name:'Priority',
+        selector: row=>row.priority,
+        sortable:true
+    },
+    {
+      name:'Actions',
+      cell:(row)=><ActionMenu row={row}/>,
+      ignoreClick:true,
+      selector: row=>row.actions,
+      sortable:true
+    },
+  ];
+  export default columns;
